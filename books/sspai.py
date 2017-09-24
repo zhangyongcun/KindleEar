e2f743afe2da6d2139d0316fd08308c3738cc210
zhangyongcun <zhangyongcun521@gmail.com>
2017-09-25 00:30:18 +0800

    remove some rss add sspi

---
+++ books/sspai.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from base import BaseFeedBook
+
+def getBook():
+    return sspai
+
+class ZhihuDaily(BaseFeedBook):
+    title                 = u'少数派'
+    description           = u'少数派 数码'
+    language              = 'zh-cn'
+    feed_encoding         = "utf-8"
+    page_encoding         = "utf-8"
+    mastheadfile = "mh_zhihudaily.gif"
+    coverfile = "cv_zhihudaily.jpg"
+    oldest_article        = 1
+    feeds = [
+            (u'少数派', 'https://sspai.com/feed', True)
+           ]
